3164e50e4a1e92026c00204b1d0876912c8e023f
Melchior Crinier <66031896+mlchior@users.noreply.github.com>
2022-04-02 13:19:59 +0200

    Creation d'une propriete CustomProperties

--- src/main/java/com/openclassrooms/webapp/WebappApplication.java
+++ src/main/java/com/openclassrooms/webapp/WebappApplication.java
@@ -1,13 +1,22 @@
 package com.openclassrooms.webapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizerBeanPostProcessor;
 
 @SpringBootApplication
-public class WebappApplication {
+public class WebappApplication implements CommandLineRunner {
 
+	@Autowired
+	private CustomProperties props;
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(props.getApiUrl());
+	}
 }
